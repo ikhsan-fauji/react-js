7b6aa325d7aa878a5c8dadada524169ac41b43e1
Ikhsan Fauji <ikhsanfauji.dev@gmail.com>
2021-03-16 22:41:39 +0700

    Greeting

---
+++ src/sample/Hello.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class Hello extends Component {
+    render() {
+        return (
+            <div>
+                Hello { this.props.name }
+            </div>
+        );
+    }
+}
+
+export default Hello;

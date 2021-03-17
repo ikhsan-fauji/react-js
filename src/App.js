0cd310d1bdd8e3ee0a6d754399fe5104cb8b4f7d
Ikhsan Fauji <ikhsanfauji.dev@gmail.com>
2021-03-17 23:18:32 +0700

    Hello sample

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Hello from './sample/Hello';
+
 function App() {
   return (
     <div className="App">
       <h1>Ikhsan Fauji - React JS</h1>
+      <Hello name="Ikhsan Fauji"/>
     </div>
   );
 }
